96ba5f820b6e689a06b532ee4da76362f1886b0f
cmdrumright <cdrumright@protonmail.com>
2025-12-10 15:41:36 -0600

    - expand the order response and add prices - updated fetch to fully expand the foreign keys - added order price formula to interpolation

--- scripts/orders.js
+++ scripts/orders.js
@@ -1,10 +1,11 @@
 export const orders = async () => {
-    const response = await fetch("http://localhost:8088/orders")
+    const response = await fetch("http://localhost:8088/orders?_expand=metal&_expand=style&_expand=size")
     const orders = await response.json()
 
     let ordersHTML = orders.map((order) => {
+        const orderPrice = order.metal.price + order.style.price + order.size.price
         return `
-            Order #${order.id} 
+            Order #${order.id} cost $${orderPrice}
         `
     }).join("</br>")
 
